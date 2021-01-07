a17203d3110efa450572e6973c9e606e3791c6a1
umedith <umedith03@gmail.com>
2021-01-07 19:17:30 +0200

    structured the application

---
+++ galleria/models.py
@@ -0,0 +1,98 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=15) 
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()    
+
+    def delete_category(self):
+        Category.objects.filter(id = self.pk).delete()
+    
+    def update_category(self, **kwargs):
+        self.objects.filter(id = self.pk).update(**kwargs)
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=20) 
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()    
+
+    def delete_location(self):
+        Location.objects.filter(id = self.pk).delete()
+   
+    def update_location(self, **kwargs):
+        self.objects.filter(id = self.pk).update(**kwargs)     
+        
+# iterable tuple to use as choices of category field
+ORDER_CHOICES = (
+    ('11', 'Vegetation'),
+    ('12', 'Trees'),
+    ('13', 'Nature'),
+    ('14', 'Work'),
+    ('15', 'Painting'),
+    ('16', 'Animals'),
+    ('17', 'Love'),
+    ('18', 'Flowers'),
+    ('19', 'Business'),
+    ('20', 'Entertainment'),
+    ('21', 'Cats'),
+    ('22', 'Car'),
+    ('23', 'People'),
+    ('24', 'Travel'),
+)
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'pics/', null = True)
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=50)
+    category = models.ForeignKey('Category', on_delete = models.CASCADE, null='True', blank=True)
+    location = models.ForeignKey('Location', on_delete = models.CASCADE, null='True', blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()   
+
+    def delete_image(self):
+        Image.objects.filter(id = self.pk).delete() 
+    
+    def update_image(self, **kwargs):
+        self.objects.filter(id = self.pk).update(**kwargs)       
+
+    @classmethod
+    def all_pics(cls):
+        pics = cls.objects.all()
+        return pics 
+
+    @classmethod
+    def pic_locations(cls):
+        pics = cls.objects.order_by('location')
+        return pics 
+
+    @classmethod
+    def pic_categories(cls):
+        pics = cls.objects.order_by('category')
+        return pics 
+
+    @classmethod
+    def get_pic(cls, id):
+        pic = cls.objects.get(id=id)
+        return pic
+
+    @classmethod
+    def search_by_category(cls, search_input):
+        images = cls.objects.filter(category__name__icontains=search_input)
+        return images        
+
+    class Meta:
+        ordering = ['name']
---
+++ static/js/scripts.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+
+    $('.category-button').click(function(event) {
+      event.preventDefault()
+      var btn_value = $(this).text()
+  
+      $.ajax({
+        'url': 'category/',
+        'type': 'GET',
+        'data': {
+          category_name: btn_value
+        },
+        'success': function(resp) {
+          $('.container-fluid').empty()
+          $('.container-fluid').html(resp)
+        },
+  
+      })
+    })
+    
+    $('option').click(function() {
+      if ($(this).is(':selected')) {
+        option_value = $(this).text()
+        $.ajax({
+          'url': 'location/',
+          'type': 'GET',
+          'data': {
+            location_name: option_value
+          },
+          'success': function(resp) {
+            $('.container-fluid').empty()
+            $('.container-fluid').html(resp)
+          },
+  
+        })
+      }
+  
+    })
+  
+  
+  $('.myimage').click(function(){
+      $('#myModal').css('display', "block")
+      $("#modal-con").attr('src',$(this).attr('src'))
+      $('#caption').html()= $(this).attr('alt')
+  })
+  
+  $('button#details').click(function() {
+  
+    $('#modal-details').css('display', 'block')
+  
+    img_d = $(this).data("description");
+  
+    loc_d = $(this).data("location");
+  
+    cat_d = $(this).data("category");
+  
+  
+    $(".oneone").text(img_d);
+  
+    $(".twotwo").text(loc_d);
+  
+    $(".threethree").text(cat_d);
+  
+  
+  })
+  
+  $(".close").click(function() {
+    $('#myModal').css('display', "none");
+    $('#modal-details').css('display', 'none')
+  })
+  })
