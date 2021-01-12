8cb613bd2e0b29b0da57a9c20928bbd53f337f08
umedith <umedith03@gmail.com>
2021-01-12 11:01:11 +0200

    updated the project

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/edith/Documents/moringa/galleria/virtual"
+set -gx VIRTUAL_ENV "/home/edith/Documents/moringa/galleria-app/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- virtual/bin/django-admin.py
+++ virtual/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/edith/Documents/moringa/galleria/virtual/bin/python
+#!/home/edith/Documents/moringa/galleria-app/virtual/bin/python
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
